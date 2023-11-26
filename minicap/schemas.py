b3146f0b6cc0c4b16289eb473ee9b956bef664a0
Gabriel Claudiu Georgiu <gabriel@claudiugeorgiu.com>
2023-11-26 23:53:22 +0100

    Fix pydantic warning

--- minicap/schemas.py
+++ minicap/schemas.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, Field
 
 
 class CaptchaValidationRequest(BaseModel):
-    id: str = Field(..., example="bca4cfaa-11a3-4577-8dc6-986d283012a8")
-    text: str = Field(..., example="8tRTu5")
+    id: str = Field(..., examples=["bca4cfaa-11a3-4577-8dc6-986d283012a8"])
+    text: str = Field(..., examples=["8tRTu5"])
 
 
 class CaptchaValidationResponse(BaseModel):
-    status: int = Field(..., example=200)
-    message: str = Field(..., example="CAPTCHA validated successfully")
+    status: int = Field(..., examples=[200])
+    message: str = Field(..., examples=["CAPTCHA validated successfully"])
